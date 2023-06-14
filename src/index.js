93f4ea985eb1ef179aac05a8d798b40d6329ec51
tlondono25 <124910975+tlondono25@users.noreply.github.com>
2023-06-13 19:52:46 -0500

    version 1.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 //star the app this is the main file
+
+const app = require('./server');
+
+app.listen(app.get('port'), () => {
+    console.log('server on port ', app.get('port'))
+});
--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
 //code of the server express
+const express = require('express');
+const path = require('path');
+
+//initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+//set the port
+app.set('views',path.join(__dirname,'views'));
+//set de location of views
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+//become objects in json
+//global variables
+
+//routes
+app.get('/',(req, res) =>{
+    res.send('hello world');
+});
+//main route
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+//use the folder public
+
+module.exports = app;
